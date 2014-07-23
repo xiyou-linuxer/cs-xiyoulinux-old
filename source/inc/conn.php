378e4e468e3c0f49ee44305e9d01922bd30cbb57
jensyn <zhangyongjun369@gmail.com>
2014-07-23 19:11:36 +0800

    更新写信前端

--- source/inc/conn.php
+++ source/inc/conn.php
@@ -9,10 +9,10 @@ class Csdb{		//封装成类,对外目前就一个query接口
 	private $charset;
 	
 	public function __construct(){	//自动初始化
-		$this->host = 'xx';
-		$this->user = 'xx';
-		$this->pwd = 'xx';
-		$this->dbName = 'xx';
+		$this->host = 'localhost';
+		$this->user = 'cs_linux';
+		$this->pwd = 'pwd_linux';
+		$this->dbName = 'cs_linux';
 		$this->charset = 'utf8';		
 		$this->connect($this->host,$this->user,$this->pwd,$this->dbName,$this->charset);
 	}
