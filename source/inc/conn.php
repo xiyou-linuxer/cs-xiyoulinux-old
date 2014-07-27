0a3ae7e401c58f7e2f421ca792ce4d5a94e8001b
wang <630841816@qq.com>
2014-07-27 21:28:23 +0800

    Update conn.php

--- source/inc/conn.php
+++ source/inc/conn.php
@@ -10,9 +10,9 @@ class Csdb{		//封装成类,对外目前就一个query接口
 	
 	public function __construct(){	//自动初始化
 		$this->host = 'localhost';
-		$this->user = 'root';
-		$this->pwd = 'perfect456';
-		$this->dbName = 'linux_project';
+		$this->user = '***';
+		$this->pwd = '******';
+		$this->dbName = '*****';
 		$this->charset = 'utf8';		
 		$this->connect($this->host,$this->user,$this->pwd,$this->dbName,$this->charset);
 	}
