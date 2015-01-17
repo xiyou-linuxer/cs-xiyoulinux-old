5960ea54eace589d7a302f2dc99d4d0d09f9dade
wangduoxiong <630841816@qq.com>
2015-01-17 16:30:36 +0800

    修改部分文件格式

--- source/load_deluser.php
+++ source/load_deluser.php
@@ -1,27 +1,8 @@
 <?php
-error_reporting(E_ALL^E_NOTICE^E_WARNING);
-
 include_once('init.php');
 include('header.php');
 include('aside.php');
 include('chat.php');
 include('footer.php');
-
-$style_list = array (
-'js/chosen/chosen.css'
-);
-$tpl->assign('style_list', $style_list);
-
-$script_list = array (
-    'js/chosen/chosen.jquery.min.js',
-    'js/admin_deluser.js'
-);
-$tpl->assign('script_list', $script_list);
-
-$tpl->display('header.html');
-$tpl->display('aside.html');
-$tpl->display('admin_deluser.html');
-$tpl->display('chat.html');
-$tpl->display('footer.html');
-
+$tpl->display('admin_deluser.tpl');
 ?>
--- source/load_reflash.php
+++ source/load_reflash.php
@@ -1,26 +1,8 @@
 <?php
-
 include_once('init.php');
 include('header.php');
 include('aside.php');
 include('chat.php');
 include('footer.php');
-
-$style_list = array (
-'js/chosen/chosen.css'
-);
-$tpl->assign('style_list', $style_list);
-
-$script_list = array (
-    'js/chosen/chosen.jquery.min.js',
-    'js/admin_reflash.js'
-);
-$tpl->assign('script_list', $script_list);
-
-$tpl->display('header.html');
-$tpl->display('aside.html');
-$tpl->display('admin_reflash.html');
-$tpl->display('chat.html');
-$tpl->display('footer.html');
-
+$tpl->display('admin_reflash.tpl');
 ?>
