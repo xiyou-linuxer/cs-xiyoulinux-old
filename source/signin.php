3c2720c00c59d7370e884475db9d0fdfc52fa770
Jensyn <zhangyongjun369@gmail.com>
2015-03-20 20:12:32 +0800

    更新signin.php，新增登录之后自后跳转上次访问页面的功能

--- source/signin.php
+++ source/signin.php
@@ -2,10 +2,12 @@
     session_start();
     include('smarty.php');
 
+    $referer_uri = ($_SERVER["HTTP_REFERER"]) ? $_SERVER["HTTP_REFERER"] : "index.php";
     if(isset($_SESSION['uid']) ) {
-        header('location: index.php');
+        header('location: ' . $referer_uri);
         exit;
     }
 
+    $smarty->assign('referer_uri', $referer_uri);
     $smarty->display('signin.tpl');
 ?>
